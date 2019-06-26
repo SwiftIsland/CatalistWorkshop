342277dc916addcd244480e133aa781bb8df2e88
Benedikt Terhechte <terhechte@gmail.com>
2019-06-26 21:39:26 +0200

    Catalyst: Hide UITabbar add NSToolbar
    This replaces the tabbar at the bottom with a proper
    macOS toolbar at the top

---
+++ Nerau/Nerau-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Nerau-Bridging-Header.h
+//  Nerau
+//
+//  Created by terhechte on 26.06.19.
+//  Copyright © 2019 Benedikt Terhechte. All rights reserved.
+//
+
+#ifndef Nerau_Bridging_Header_h
+#define Nerau_Bridging_Header_h
+
+#import <Foundation/Foundation.h>
+#import <UIKit/NSToolbar+UIKitAdditions.h>
+
+
+#endif /* Nerau_Bridging_Header_h */
