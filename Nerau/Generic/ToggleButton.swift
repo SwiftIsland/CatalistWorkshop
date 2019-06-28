136bfd64886ba6950f3e10f9b5a398fd0317a9a1
Benedikt Terhechte <terhechte@gmail.com>
2019-06-28 10:07:35 +0200

    Use SFSymbol for macOS and iOS13 to auto support dark mode
    This uses SFSymbol where available to get nicely tinted
    colors. In order to still support iOS 12 we also keep
    the old images that are light mode only.

--- Nerau/Generic/ToggleButton.swift
+++ Nerau/Generic/ToggleButton.swift
@@ -12,9 +12,28 @@ public final class ToggleButton: UIButton {
     
     public override func awakeFromNib() {
         super.awakeFromNib()
+        #if targetEnvironment(UIKitForMac)
+        iconsSFSymbol()
+        #else
+        if #available(iOS 13, *) {
+            iconsSFSymbol()
+        } else {
+            iconsDefault()
+        }
+        #endif
+        addTarget(self, action: #selector(didToggle(sender:)), for: .touchUpInside)
+    }
+    
+    @available(iOS 13.0, *)
+    @available(UIKitForMac 13.0, *)
+    private func iconsSFSymbol() {
+        setImage(UIImage(systemName: "star"), for: .normal)
+        setImage(UIImage(systemName: "star.fill"), for: .selected)
+    }
+    
+    private func iconsDefault() {
         setImage(UIImage(named: "star_inactive"), for: .normal)
         setImage(UIImage(named: "star_active"), for: .selected)
-        addTarget(self, action: #selector(didToggle(sender:)), for: .touchUpInside)
     }
     
     @objc func didToggle(sender: ToggleButton) {
